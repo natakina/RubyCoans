5b0d44685b213d98ed8ee0b97018aa7e544a83d6
natalia-zykina <natalia.zykina@agileengine.com>
2019-07-25 18:09:31 +0300

    #3RubyCoans-160/282

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  value = if a == b && b == c
+            :equilateral
+          else if a == b || b ==c || a==c
+                 :isosceles
+               else
+                 :scalene
+               end
+          end
 end
 
 # Error class used in part 2.  No need to change this code.
