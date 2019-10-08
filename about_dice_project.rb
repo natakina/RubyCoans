401147d5adef15e3348f4be1e118f250e80d659a
natakina <natakina7@gmail.com>
2019-10-08 18:16:10 +0300

    216/282- classes

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,17 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def roll(max)
+    @values = []
+    max.downto(1) do |i|
+      @values.push(1 + rand(i + 1))
+    end
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
